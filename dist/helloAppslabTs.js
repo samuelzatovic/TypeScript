4f80cd6c9f8198a545e56b436c9bb98136553f41
samuel zaťovič <szatovic@gmail.com>
2020-10-21 21:07:25 +0200

    Equality

--- dist/helloAppslabTs.js
+++ dist/helloAppslabTs.js
@@ -1,3 +1,12 @@
-let message = "Hello World";
-console.log(message);
+function equality(a, b, c) {
+    if (a == b || a == c) {
+        if (b == c)
+            return 3;
+        return 2;
+    }
+    if (b == c)
+        return 2;
+    return 0;
+}
+console.log(equality(3, 4, 5));
 //# sourceMappingURL=helloAppslabTs.js.map
--- helloAppslabTs.ts
+++ helloAppslabTs.ts
@@ -1,2 +1,10 @@
-let message:string = "Hello World" 
-console.log(message)
+function equality(a:number, b:number, c:number):number
+{
+    if(a == b || a == c){
+        if(b == c) return 3;
+        return 2;}
+
+    if(b == c) return 2;
+    return 0;
+}
+console.log(equality(3,4,5))
