1da954c407940f087659f811fd293a1dc536098e
samuel zaťovič <szatovic@gmail.com>
2020-10-21 21:00:40 +0200

    Snail

--- dist/helloAppslabTs.js
+++ dist/helloAppslabTs.js
@@ -1,3 +1,6 @@
-let message = "Hello World";
-console.log(message);
+function totalDistance(height, length, steps) {
+    let totalDistance = (steps / height) * (length + height);
+    return totalDistance;
+}
+console.log(totalDistance(0.2, 0.4, 100));
 //# sourceMappingURL=helloAppslabTs.js.map
--- helloAppslabTs.ts
+++ helloAppslabTs.ts
@@ -1,2 +1,7 @@
-let message:string = "Hello World" 
-console.log(message)
+function totalDistance (height:number, length:number, steps:number):number
+{
+    let totalDistance = (steps/height) * (length + height);
+    return totalDistance;
+}
+
+console.log(totalDistance(0.2, 0.4, 100))
