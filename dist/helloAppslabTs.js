9a46ea59ae1da5c88bff50d4e27f1f1017e69762
samuel zaťovič <szatovic@gmail.com>
2020-10-26 22:11:13 +0100

    Rook

--- dist/helloAppslabTs.js
+++ dist/helloAppslabTs.js
@@ -1,5 +1,5 @@
-function checkEnding(a, b) {
-    return (a.endsWith(b));
+function rook(a, b) {
+    return a.charAt(0) == b.charAt(0) || a.charAt(1) == b.charAt(1);
 }
-console.log(checkEnding("ahoj", "oj"));
+console.log(rook("H4", "G2"));
 //# sourceMappingURL=helloAppslabTs.js.map
--- helloAppslabTs.ts
+++ helloAppslabTs.ts
@@ -1,5 +1,5 @@
-function checkEnding(a:string, b:string)
+function rook(a:string, b:string)
 {
-    return (a.endsWith(b));
+    return a.charAt(0) == b.charAt(0) || a.charAt(1) == b.charAt(1);
 }
-console.log(checkEnding("ahoj", "oj"));
+console.log(rook("H4", "G2"));
